2aaee5230049149f908d127499add44ea6de399b
Daniele Pellegrini <10761843+dmpellegrini@users.noreply.github.com>
2023-01-14 12:50:39 -0500

    NPM installed trash-cli

--- setup_node.zsh
+++ setup_node.zsh
@@ -15,6 +15,7 @@ npm install --global @ionic/cli
 npm install --global typescript
 npm install --global json-server
 npm install --global http-server
+npm install --global trash-cli
 
 echo "Global NPM Packages Installed:"
 npm list --global --depth=0
