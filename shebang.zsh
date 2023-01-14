2205ed715273300e9139115b1433360ee0bd646f
Daniele Pellegrini <10761843+dmpellegrini@users.noreply.github.com>
2023-01-14 05:55:15 -0500

    Renamed test_exists to shebang

--- shebang.zsh
+++ shebang.zsh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 
 echo "\n<<< Running $0 >>>\n"
+echo "ZSH_VERSION = $ZSH_VERSION"
+echo "The current shell is: $(ps $$ -ocomm=)" 
 
 exists brew && echo "excellent!!!" || echo "bogus"
 exists nonexistent && echo "excellent!!!" || echo "bogus"
