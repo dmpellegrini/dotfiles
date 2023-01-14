9eff55202b226d8ed0fd4f950fcdee1c93bd2557
Daniele Pellegrini <10761843+dmpellegrini@users.noreply.github.com>
2023-01-14 07:39:47 -0500

    Changed setup_zsh to redirect to black hole

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells'
+echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 
 echo "Enter user password to change login shell"
 chsh -s '/usr/local/bin/zsh'
