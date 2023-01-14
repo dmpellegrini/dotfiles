d5c61ada8dd3eb6de2f1572de8ffd3c997c0e167
Daniele Pellegrini <10761843+dmpellegrini@users.noreply.github.com>
2023-01-14 07:29:39 -0500

    Created script to change shell to zsh

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -1,3 +1,7 @@
 #!/usr/bin/env zsh
 
-echo "\n<<< Starting zsh Setup >>>\n"
+echo "Enter superuser (sudo) password to edit /etc/shells"
+echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells'
+
+echo "Enter user password to change login shell"
+chsh -s '/usr/local/bin/zsh'
