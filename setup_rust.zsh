76214f24b4503dc128bd48c2c244f61c10addfff
Daniele Pellegrini <10761843+dmpellegrini@users.noreply.github.com>
2023-01-27 18:18:38 -0500

    Added rust installation file

---
+++ setup_rust.zsh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting Homebrew Setup >>>\n"
+
+if exists rustup; then
+  echo "rustup exists, skipping install"
+else
+  echo "rustup doesn't exist, continuing with install"
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+
