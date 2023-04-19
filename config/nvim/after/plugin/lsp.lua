409b4111036f2a75a44d22d58ee3d40146bc2d75
Daniele Pellegrini <10761843+dmpellegrini@users.noreply.github.com>
2023-04-19 12:44:13 -0400

    Simplified LSP setup

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -12,14 +12,16 @@ lsp.setup()
 
 -- Make sure you setup `cmp` after lsp-zero
 
+local luasnip = require("luasnip")
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
 	sources = {
-		{name = 'nvim_lsp'},
+		{name = 'nvim_lsp', keyword_length = 2},
 		{name = 'luasnip', keyword_length = 2},
-		{name = 'buffer', keyword_length = 3},
+		{name = 'nvim_lua'},
+		{name = 'buffer'},
 		{name = 'path'},
 	},
 	mapping = {
@@ -28,37 +30,37 @@ cmp.setup({
             behavior = cmp.ConfirmBehavior.Insert,
             select = true,
         }),
-		-- ['<Tab>'] = cmp_action.luasnip_supertab(),
-        -- ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-                -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
-                -- they way you will only jump inside the snippet region
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            elseif has_words_before() then
-                cmp.complete()
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
+		['<Tab>'] = cmp_action.luasnip_supertab(),
+        ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+        -- ["<Tab>"] = cmp.mapping(function(fallback)
+        --     if cmp.visible() then
+        --         cmp.select_next_item()
+        --         -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
+        --         -- they way you will only jump inside the snippet region
+        --     elseif luasnip.expand_or_jumpable() then
+        --         luasnip.expand_or_jump()
+        --     elseif has_words_before() then
+        --         cmp.complete()
+        --     else
+        --         fallback()
+        --     end
+        -- end, { "i", "s" }),
 
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
+        -- ["<S-Tab>"] = cmp.mapping(function(fallback)
+        --     if cmp.visible() then
+        --         cmp.select_prev_item()
+        --     elseif luasnip.jumpable(-1) then
+        --         luasnip.jump(-1)
+        --     else
+        --         fallback()
+        --     end
+        -- end, { "i", "s" }),
     },
     snippet = {
       expand = function(args)
         require('luasnip').lsp_expand(args.body)
-        require('nvim_lsp').lsp_expand(args.body)
-        require('path').lsp_expand(args.body)
+        -- require('nvim_lsp').lsp_expand(args.body)
+        -- require('path').lsp_expand(args.body)
       end,
     },
     experimental = {
